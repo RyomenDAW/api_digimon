c83344f898776f9a61286b987dea938cf24d4b53
Israel Hernandez <israel.hernandez.chacon.al@iespoligonosur.org>
2025-02-04 23:19:11 +0100

    Terminado el home arreglar calidad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';  // Asegúrate de importar HttpClientModule
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, HomeComponent],  // Asegúrate de incluirlo aquí],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';  // 🔥 Agrega esta importación
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient()  // 🔥 Esta es la línea que debes agregar
+  ]
 };
